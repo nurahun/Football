551af86c1b1ba3837a6c7b9b8459f34b37c61e30
nur <altun.darwaza@gmail.com>
2025-02-13 17:39:20 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Nur Khadr on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
